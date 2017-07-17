86a631f02786ce51fa593750cf6a2ee24c273e49
Ali <iloveyii@yahoo.com>
2017-07-18 01:32:52 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+emailer
+=======
+
+A Symfony project created on July 18, 2017, 1:12 am.
---
+++ web/info.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: alex
+ * Date: 2017-07-18
+ * Time: 01:25
+ */
+phpinfo();
