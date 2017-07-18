248fd63debdfabdbcd89f0be00ab4813af0daf43
Ali <iloveyii@yahoo.com>
2017-07-19 00:50:32 +0200

    Added otphp

--- src/AppBundle/Controller/OtpController.php
+++ src/AppBundle/Controller/OtpController.php
@@ -2,20 +2,34 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Entity\Otp;
+use AppBundle\Form\EmailType;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
 
 class OtpController extends Controller
 {
     /**
      * @Route("/otp/create", name="otp_create")
      */
-    public function indexAction(Request $request)
+    public function createAction(Request $request)
     {
+        $otp = new Otp();
+        $form = $this->createForm(new EmailType(), $otp);
+        $form->handleRequest($request);
+
+        //$totp = new \OTPHP\TOTP("JBSWY3DPEHPK3PXP");
+
+        if($form->isSubmitted() && $form->isValid()) {
+            return new Response('Email sent');
+        }
+
         // replace this example code with whatever you need
-        return $this->render('default/index.html.twig', array(
-            'base_dir' => realpath($this->container->getParameter('kernel.root_dir').'/..').DIRECTORY_SEPARATOR,
+        return $this->render('otp/create.html.twig', array(
+            'form' => $form->createView()
         ));
     }
 }
